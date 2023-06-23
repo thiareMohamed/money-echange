925ce9e992d4497a799d1dab99ed9a1aed762ab9
thiareMohamed <thiaremohamed.mt@gmail.com>
2023-06-23 13:17:23 +0000

    add hero

---
+++ app/components/hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './navbar'
+
+function Hero() {
+  return (
+    <header className="flex min-h-screen flex-col justify-center p-24 px-60 space-y-5">
+        <h1 className="text-white font-semibold text-4xl">Welcome to Money Echange</h1>
+        <p className="text-gray-300 text-xl font-light">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. sed do eismod <br />
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. sed do eismod
+        </p>
+    </header>
+  )
+}
+
+export default Hero
